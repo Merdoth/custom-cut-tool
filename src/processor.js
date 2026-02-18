469606f9a7622d553e2cb3dedcc1dc33068baa77
merdoth <ucheya97@gmail.com>
2026-02-18 23:48:13 +0100

    Add processInput function for processor.js

---
+++ src/processor.js
@@ -0,0 +1,16 @@
+
+
+export const processInput = (input, delimiter, fieldNumbers) => {
+  const lines = input.split("\n");
+
+  return lines
+    .filter(line => line.length > 0)
+    .map(line => {
+      const columns = line.split(delimiter);
+
+      return fieldNumbers
+        .map(n => columns[n - 1])
+        .join(delimiter);
+    })
+    .join("\n");
+};
